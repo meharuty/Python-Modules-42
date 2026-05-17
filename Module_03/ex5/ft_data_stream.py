496078a4b09c46f84b7b31ee443eeea0d0ecd009
Meline Harutyunyan <meharuty@c2r10s2.42yerevan.am>
2026-05-17 21:20:07 +0400

    push

--- Module_03/ex5/ft_data_stream.py
+++ Module_03/ex5/ft_data_stream.py
@@ -1,7 +1,8 @@
+from typing import Generator
 import random
 
 
-def gen_event():
+def gen_event() -> Generator[tuple[str, str], None, None]:
     players = [
         "Aria", "Leo", "Maya", "Noah", "Zoe",
         "Ethan", "Liam", "Nora", "Ava", "Kai"
@@ -15,10 +16,24 @@ def gen_event():
     i = -1
     while True:
         i += 1
-        yield (
-            f"Event {i}: Player {random.choice(players)} "
-            f"did {random.choice(actions)}"
-            )
+        yield (random.choice(players), random.choice(actions))
 
 
-print(gen_event())
+print("=== Game Data Stream Processor ===")
+g = gen_event()
+
+for i in range(1000):
+    tp = (next(g))
+    print(f"Event {i}: Player {tp[0]} did action {tp[1]}")
+
+ls = []
+a = gen_event()
+for i in range(10):
+    ls.append(next(a))
+
+print("Built list of 10 events:", ls)
+for i in range(10):
+    k = random.choice(ls)
+    print("Got event from list:", k)
+    ls.remove(k)
+    print("Remains in list:", ls)
