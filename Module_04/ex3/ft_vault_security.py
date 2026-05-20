6902c7113719feaa7f538716e165aa42dd6863e2
Meline <melineharutyunyan168@gmail.com>
2026-05-20 13:05:19 +0400

    module 4 is done

---
+++ Module_04/ex3/ft_vault_security.py
@@ -0,0 +1,40 @@
+def secure_archive(filename: str, mode: str = "read",
+                   content: str = "") -> tuple:
+    try:
+        if mode == "read":
+            with open(filename) as f:
+                return (True, f.read())
+
+        elif mode == "write":
+            with open(filename, "w") as f:
+                f.write(content)
+                return (True, "Content successfully written to file")
+
+    except FileNotFoundError:
+        return (False, f"[Errno 2] No such file or directory: '{filename}'")
+    except PermissionError:
+        return (False, f"[Errno 13] Permission denied: '{filename}'")
+    if (mode == "read"):
+        return (True, f.read())
+    else:
+        f.write(content)
+        return (True, "Content successfully written to file")
+
+
+print("=== Cyber Archives Security ===\n")
+print("Using 'secure_archive' to read from a nonexistent file:")
+tp = secure_archive("c.txt", 'read')
+print(tp)
+print()
+print("Using 'secure_archive' to read from an inaccessible file:")
+tp = secure_archive("b.txt", "read")
+print(tp)
+print()
+print("Using 'secure_archive' to read from a regular file:")
+tp = secure_archive("a.txt", "read")
+print(tp)
+print()
+
+print("Using 'secure_archive' to write previous content to a new file:")
+tp = secure_archive("a.txt", "write", "Melin")
+print(tp)
